6ee2310b9d603d72fae638c67ccb9837ea7d09a1
helios12138 <67767201+helios12138@users.noreply.github.com>
2020-10-20 20:23:17 +0800

    Add files via upload

---
+++ jisuanqi/pages/index/index.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    
+    motto: '简易计算器☞',
+    userInfo: {},
+    defaultSize: 'default',
+    disabled: false,
+    iconType:'info_circle'
+  },
+  //事件处理函数
+  toupper: function(){
+    console.log("触发了toupper");
+  },
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  toCalc:function(){
+    wx.navigateTo({
+      url:'../calc/calc'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad');
+    var that = this
+  	//调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
